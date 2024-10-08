6ca7bd7bf287e64eaf6370579cd328cc54e4a6f0
Dusk <dusk.argentum@gmail.com>
2024-10-08 08:00:16 -0400

    v.0.0.0d | "Finalized" (lol) points system to determine most likely character and also a rudimentary experience adding stuff. Also, a default reference json for 5e experience curve is here, with custom additions for levels up to 30

--- cogs/experience.py
+++ cogs/experience.py
@@ -10,25 +10,17 @@ import json
 
 import re
 
+from datetime import datetime
 
-# on_message > ProcessingChecks > ConfigurationChecks > RewardExperience
+import time
 
-# ProcessingChecks narrows down whether the message is worth processing. Is it from a non-bot user? Is it in a channel
-# that is not in the OOC category? Does the message start with a (/end with a )? And other checks. If all of them pass,
-# then the message is processed using the server's configuration.
-
-# ConfigurationChecks applies more checks and modifiers to the message, including whether it should grant experience
-# based on a user's role (eg. for the No EXP role), and other configuration parameters.
-
-# RewardExperience processes all the information from ConfigurationChecks to determine the final EXP amount rewarded
-# and to which character the experience is rewarded.
 
 class Experience(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @staticmethod
-    async def processing_checks(self, ctx):
+    async def processing(self, ctx):
         if ctx.author.id == self.bot.user.id:
             return
         if ctx.author.bot:
@@ -40,15 +32,15 @@ class Experience(commands.Cog):
         # TODO: Check for if sender is active player?
         # TODO: Surely, there's more reasons why a message wouldn't be processed at all.
         print("Processing complete. Moving to config...")
-        await Experience.config_checks(self, ctx=ctx)
+        await Experience.config(self, ctx=ctx)
 
     @staticmethod
-    async def config_checks(self, ctx):
+    async def config(self, ctx):
         with open("config.json", "r") as config:
             data = json.load(config)
             config.close()
         config = data["config"][f"{ctx.guild.id}"]["experience"]
-        dm_choose = None
+        dm_choose = False
         dm_roles = []
         if config["ooc_messages"]["start"] is not False and config["ooc_messages"]["end"] is not False:
             pattern = f"\\{config['ooc_messages']['start']}.*\\{config['ooc_messages']['end']}"
@@ -67,14 +59,14 @@ class Experience(commands.Cog):
                 if role.id in config["ignored_roles"]:
                     return
         if config["dm"]["choose_experience"] is not False:
-            for role in config["dm_roles"]:
+            for role in config["dm"]["roles"]:
                 dm_roles.append(role)
             dm_choose = True
-        print("Configuration complete. Moving to rewarding...")
-        await Experience.rewarding(self, ctx=ctx, dm_choose=dm_choose, dm_roles=dm_roles)
+        print("Configuration complete. Moving to determination...")
+        await Experience.determination(self, ctx=ctx, dm_choose=dm_choose, dm_roles=dm_roles)
 
     @staticmethod
-    async def rewarding(self, ctx, dm_choose, dm_roles):
+    async def determination(self, ctx, dm_choose, dm_roles):
         try:
             con = sqlite3.connect("characters.db", timeout=30.0)
         except OperationalError:
@@ -86,43 +78,28 @@ class Experience(commands.Cog):
         if not characters:
             return
         rewarded = None
+        highest_points = 0
+        next_experience = 0
         for characters_list in characters:
             character = []
             for value in characters_list:
                 character.append(value)
-            if character[6] == 1:  # Global character check. This is the default fallback and intended to be overwritten
-                # by the user. Only one character can be a global character per player. At least one character must have
-                # the global character flag at all times.
-                rewarded = character[1]
-                # TODO: grants 1 point
-            # The thought process is that global character should overwrite names, but if the channel is specifically
-            # set to be for a specific character, then the channel will take precedence.
-            # It is possible to not have a global character set. A player with a global character set can still
-            # gain EXP on the non-global character if it is... OK, it should be at the top.
-            # In most circumstances, one player's character will not talk about the other characters. However,
-            # even if they are mentioned, there are other safeguards, such as the channel being a preferred channel.
-            # I should add another method of checking to make sure the exp is going to the correct character after or
-            # just before the channels check to add a second layer.
-            if character[7] == 1:  # Active character check. This is a forced override intended for players who are
-                # extraordinarily vigilant about switching their characters. It bypasses all other checks, excluding
-                # the DM preferred character check.
-                rewarded = character[1]
-                break
-                # TODO: grants 20 points?
+            character.append(0)
+            if character[6] == 1:  # GLOBAL check. There must always be one GLOBAL character per player. This adds
+                # one point to the specified GLOBAL character's likelihood to break a tie of zeroes.
+                character[23] += 1
+            if character[7] == 1:  # ACTIVE check. Rewards players who are vigilant with switching characters by giving
+                # the character who is ACTIVE a greater likelihood without completely overriding other checks.
+                character[23] += 20
             name_search_pattern = f"{str(character[1][0]).capitalize()}{str(character[1][1:]).lower()}"
-            name_search = re.search(rf"[\s|*|\"|\']{name_search_pattern}[\s|*|\"|\'|.|,|?|!]", ctx.content)
-            if name_search is not None:  # Character name check. Searches the content of the message for a mention
-                # of the character's name, and sets the rewarded character to that character. Has the notable flaw of
-                # being able to have character experience awarded to the wrong character if the player mentions multiple
-                # of their character names in one message.
-                rewarded = character[1]  # TODO: Hold that thought. Perhaps each check could increase the likelihood for
-                # TODO: each character, and the character with the highest likelihood gets rewarded?
-                # break
-                # This should not override all other checks. Right?
-                # TODO: grants 5 points
+            name_search = re.search(rf"[\s|*|\"|\'|_]{name_search_pattern}[\s|*|\"|\'|.|,|?|!|_]", ctx.content)
+            if name_search is not None:  # NAME check. Searches the content of the message for a mention of the
+                # character's name, and adds likelihood to that character. Can trip for multiple characters on a player
+                # if their message mentions multiple of their characters, hence why its increase is so low.
+                character[23] += 5
             nicknames = character[19:22]
             nick_slice = 19
-            for nick in nicknames:  # Same as the name check, but for nicknames. The same flaw is inherent here, too.
+            for nick in nicknames:  # NICK check. Similar to NAME, but less powerful to mitigate ties and misuse.
                 if nick == "":
                     nick_slice += 1
                     continue
@@ -133,52 +110,99 @@ class Experience(commands.Cog):
                     nick_slice += 1
                     continue
                 elif nick_search is not None:
-                    rewarded = character[1]
-                    # TODO: grants 4 points
-                    # break
+                    character[23] += 4
             preferred_channels = character[9:19]
             channel_slice = 9
-            for channel in preferred_channels:  # Preferred channel check. If the message sent is in one of the
-                # character's preferred channels, that character is rewarded.
+            for channel in preferred_channels:  # CHANNEL check. Rewards players who predefine channels where that
+                # character is most likely to be, such as a house channel, or channels set before an outing. Powerful
+                # enough to be rewarding, while still enabling overwrite if a player forgets to unset channels.
                 if channel == 0:
                     channel_slice += 1
                     continue
                 if ctx.channel.id == character[channel_slice]:
-                    rewarded = character[1]
-                    break
+                    character[23] += 15
                 channel_slice += 1
-                # TODO: Grants 15 points
-            underlined_name_search = re.search(rf"_{2}{name_search_pattern}_{2}", ctx.content)
-            if underlined_name_search is not None:  # Underlined check. If a character's name is underlined anywhere
-                # in the message, that character is rewarded.
-                rewarded = character[1]
-                break
-                # TODO: grants 30 points
-            # underlined_name_pattern = f"{str(character[1][0]).capitalize()}{str(character[1][1:]).lower()}"
-        print(rewarded)
-        # TODO: Add "active" character which can be switched between..? Is that the same as or different from global?
-        # TODO: Global and above
-        # Global needs to be put somewhere where it won't overwrite preferences but will still take precedence
-        # name > nick > preferred > global > slice > choice
-        # name and nick need regex to ensure that simply mentioning the other character doesn't trip it?
-        # or other sanity matching
-        # if nick/name in regex for message, that is rewarded. Unless DM role
-        # if DM role present, check DM slice. That is rewarded unless all are 0.
-        # if channel id matches preferred for a character, that is rewarded. Unless DM role
-        # if DM role is present and DM slice are all 0, send ephem for choice.
-        # choice ephem has a 5 minute timeout. If timeout, unclaimed exp is logged for later claim. Dmess>command?
-        # if global is 1, that is rewarded. Unless DM role
-        # print(characters)
+            underlined_name_search = re.search(rf"_{r"{2}"}{name_search_pattern}_{r"{2}"}", ctx.content)
+            if underlined_name_search is not None:  # UNDERLINE check. Essentially allows a player to manually set
+                # which character obtains experience. Able to be misused, but very obvious, both in message content,
+                # and in message logs.
+                character[23] += 100
+            if dm_choose is True and dm_roles != []:  # DM check. Allows a DM to set a preferred character for obtaining
+                # experience while running adventures. Can only be overridden by UNDERLINE.
+                for role in dm_roles:
+                    if role in ctx.author.roles:
+                        if character[8] == 1:
+                            character[23] += 50
+                            break
+            for character_points in characters:  # Compares the points of characters and sets them as rewarded if theirs
+                # is the highest total.
+                if character[23] > highest_points:
+                    highest_points = character[23]
+                    rewarded = character[1]
+            print(f"{character[1]} has {character[23]} points. The highest point total is {highest_points}.")
+        print("Determination complete. Moving to rewarding...")
+        await Experience.rewarding(self, ctx=ctx, rewarded=rewarded)
+
+    @staticmethod
+    async def rewarding(self, ctx, rewarded):
+        try:
+            con = sqlite3.connect("characters.db", timeout=30.0)
+        except OperationalError:
+            return
+        cur = con.cursor()
+        cur.execute("""SELECT experience, level, tier, next_experience FROM characters \
+WHERE player_id = ? AND character_name = ?""", [ctx.author.id, rewarded])
+        values = cur.fetchall()
+        con.close()
+        value_list = []
+        for value in values[0]:
+            value_list.append(value)
+        if datetime.now(tz=None) < datetime.fromtimestamp(value_list[3], tz=None):
+            return
+        experience = value_list[0] + 1  # Here, the experience added will be modified.
+        try:
+            con = sqlite3.connect("characters.db", timeout=30.0)
+        except OperationalError:
+            return
+        cur = con.cursor()
+        cur.execute("UPDATE characters SET experience = ? WHERE player_id = ? AND character_name = ?",
+                    [experience, ctx.author.id, rewarded])
+        con.commit()
+        cur.execute("SELECT experience FROM characters WHERE player_id = ? AND character_name = ?",
+                    [ctx.author.id, rewarded])
+        final_experience = cur.fetchall()[0][0]
+        con.close()
+        with open("config.json", "r") as config:
+            data = json.load(config)
+            config.close()
+        config = data["config"][str(ctx.guild.id)]["experience"]
+        next_experience = int(time.time()) + config["time_between"]
+        try:
+            con = sqlite3.connect("characters.db", timeout=30.0)
+        except OperationalError:
+            return
+        cur = con.cursor()
+        cur.execute("UPDATE characters SET next_experience = ? WHERE player_id = ? AND character_name = ?",
+                    [next_experience, ctx.author.id, rewarded])
+        con.commit()
+        con.close()
+        with open("experience.json", "r") as experience_reference:
+            data = json.load(experience_reference)
+            experience_reference.close()
+        if final_experience > data["experience"][f"{value_list[1] + 1}"]:
+            await Experience.level(self, ctx=ctx, rewarded=rewarded)
+        print(f"Dew's experience total is {final_experience}.")
+        exp = datetime.fromtimestamp(next_experience, tz=None)
+        print(f"Dew will be able to gain experience again at {exp}")
+        # print(value_list)
+
+    @staticmethod
+    async def level(self, ctx, rewarded):
+        print("yey level. woo, etc.")
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        await Experience.processing_checks(self, ctx=ctx)
-
-    @commands.command(aliases=["wet"], brief="a", help="b", name="test2", usage="bong")
-    @commands.guild_only()
-    async def testy(self, ctx):
-        print(self.bot.user.id)
-        return
+        await Experience.processing(self, ctx=ctx)
 
 
 def setup(bot):
